b937145e81b8e7b89ec8d05f8c3d8be227896852
Vinaykumar Dubey <vinay.dubey0990@gmail.com>
2020-11-18 00:33:25 +0530

    login and registration implemented

--- src/main/java/com/nilam/dev/controller/AppController.java
+++ src/main/java/com/nilam/dev/controller/AppController.java
@@ -1,23 +1,51 @@
 package com.nilam.dev.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.nilam.dev.model.User;
+import com.nilam.dev.repository.UserRepository;
 
 @Controller
 public class AppController {
 	
+	@Autowired
+	private UserRepository repository;
+
 	@GetMapping("")
 	public String viewHomePage() {
 		return "index";
 	}
-	
+
 	@GetMapping("/register")
-	public String viewRegister() {
+	public String getRegister(Model model) {
+		model.addAttribute("user", new User());
 		return "register";
 	}
-	
+
 	@GetMapping("/login")
-	public String viewLogin() {
+	public String getLogin(Model model) {
+		model.addAttribute("user", new User());
 		return "login";
+	}
+
+	@PostMapping("/process_login")
+	public String postLogin(User user) {
+		User temp = repository.findByEmail(user.getEmail());
+		if(temp.getPassword().equals(user.getPassword())) {
+			System.out.println("yes found");
+		}else {
+			System.out.println("no");
+		}
+		return "index";
+	}
+	
+	@PostMapping("/process_register")
+	public String postRegister(User user) {
+		repository.save(user);
+		return "index";
 	}
 }
